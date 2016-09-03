1d2931cd14db2e89405158d6228db0ff9b0c5254
hans <hans@nexell.co.kr>
2016-09-03 15:43:10 +0900

    fix efuse boot option.
    Signed-off-by: hans <hans@nexell.co.kr>

--- src/nx_etacarinae_bootoption.h
+++ src/nx_etacarinae_bootoption.h
@@ -20,39 +20,51 @@
 #ifndef NX_BOOTOPTION_H
 #define NX_BOOTOPTION_H
 
+
+/* ========================= rst cfg =====================*/
+#define BOOTMODE		0
+
 #define EMMCBOOT		0
-#define	USBBOOT			1
-#define	SDFSBOOT		2
-#define	SDBOOT			3
-#define	NANDECBOOT		4
-#define	SPIBOOT			5
+#define USBBOOT			1
+#define SDFSBOOT		2
+#define SDBOOT			3
+#define NANDECBOOT		4
+#define SPIBOOT			5
 #define GMACBOOT		6
 #define STATIC			7
 
 
-#define PORTNUMBER	        5 // ~ 6
+#define PORTNUMBER		5
 
-#define SELSPIPORT		PORTNUMBER
-#define SERIALFLASHADDR		3
-#define SERIALFLASHSPEED	4
+#define SERIALFLASHADDR		3	// 0: 3 step 1: 4 step
+#define SERIALFLASHSPEED	4	// 0: 1MHz, 1: 16MHz
+#define SELSPIPORT		5	// 0, 1, 2
 
-#define SELSDPORT		PORTNUMBER
-#define eMMCBOOTMODE		3
-#define SDXCPARTITION		4
+#define eMMCBOOTMODE		3	// 0: cmd line low, 1: boot cmd(0xFFFFFFFA)
+#define SDXCPARTITION		4	//
+#define SELSDPORT		5	// 0, 1, 2: port number, 3: hispeed p 0
+
+#define NANDADDRSTEP		3	// small: 0:3, 1:4, large: 0:4, 1:5
+#define NANDTYPE		4	// 0: small, 1: large
+#define NANDPAGE		5	// 0: 2k, 1:4K, 2:8K, 3:16K ~
+
+#define ICACHE			7	// rstcfg only. 0: disable, 1: enable
+#define COREVOLTAGE		8	// rstcfg only. 0: 1.0V, 1:1.1V
 
 
-#define BOOTMODE		0
+/* ========================= efuse =====================*/
 
-#define NANDTYPE		3
-#define NANDPAGE		5
+#define NEXTTRYPORT		7	// 0, 1, 2: port number, 3: not try
+#define USE_SDFS		9	// 0: sector read, 1: file read
 
-#define NEXTTRY			16
-#define USE_FS			20
-#define NEXTPORT		18
+#define ReservedBIT		10	// reserved for future use
 
-#define DECRYPT			14
-#define ICACHE			7
 
-#define COREVOLTAGE		8
+#define NOBOOTMSG		12	// 0: boot message, 1: no boot message
+#define NOBOOT			13	// must be zero
+#define VALIDFIELD		14	// 0: valid, 1: invalid
+#define BOOTCFGUSE		15	// 0: not use, 1: use
+
+#define DECRYPT			16	// virtual flag bit.
 
 #endif
